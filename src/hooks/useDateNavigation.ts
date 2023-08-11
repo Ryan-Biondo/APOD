f2537c88e5efb85b75e38f22399db762a9862e88
Ryan Biondo <biondoryan.dev@gmail.com>
2023-08-11 06:44:25 -0600

    update isAtEndDate

--- src/hooks/useDateNavigation.ts
+++ src/hooks/useDateNavigation.ts
@@ -4,30 +4,36 @@ const useDateNavigation = () => {
   const navigate = useNavigate();
   const { date } = useParams<{ date: string }>();
 
-  const today = new Date(Date.UTC(new Date().getUTCFullYear(), new Date().getUTCMonth(), new Date().getUTCDate() - 1)).toISOString().split('T')[0];
+  const today = new Date(Date.UTC(new Date().getUTCFullYear(), new Date().getUTCMonth(), new Date().getUTCDate() )).toISOString().split('T')[0];
   const earliestDate = '1995-06-16';
 
   const isAtStartDate = date === earliestDate;
-  const isAtEndDate = date === today;
+  const isAtEndDate = date ? date >= today : false;
 
   const navigateToPreviousDate = (): Date | null => {
     if (isAtStartDate) return null;
-    const prevDate = new Date(date!);
-    prevDate.setMinutes(prevDate.getMinutes() + prevDate.getTimezoneOffset());
-    prevDate.setDate(prevDate.getDate() - 1);
-    navigate(`/details/${prevDate.toISOString().split('T')[0]}`);
-    console.log(prevDate + 'from' + date);
-    return prevDate;
+    if (date) {
+      const prevDate = new Date(date);
+      prevDate.setMinutes(prevDate.getMinutes() + prevDate.getTimezoneOffset());
+      prevDate.setDate(prevDate.getDate() - 1);
+      navigate(`/details/${prevDate.toISOString().split('T')[0]}`);
+      console.log(prevDate + ' from ' + date);
+      return prevDate;
+    }
+    return null;
   };
 
   const navigateToNextDate = (): Date | null => {
     if (isAtEndDate) return null;
-    const nextDate = new Date(date!);
-    nextDate.setMinutes(nextDate.getMinutes() + nextDate.getTimezoneOffset());
-    nextDate.setDate(nextDate.getDate() + 1);
-    navigate(`/details/${nextDate.toISOString().split('T')[0]}`);
-    console.log(nextDate + 'from' + date);
-    return nextDate;
+    if (date) {
+      const nextDate = new Date(date);
+      nextDate.setMinutes(nextDate.getMinutes() + nextDate.getTimezoneOffset());
+      nextDate.setDate(nextDate.getDate() + 1);
+      navigate(`/details/${nextDate.toISOString().split('T')[0]}`);
+      console.log(nextDate + ' from ' + date);
+      return nextDate;
+    }
+    return null;
   };
 
   return {
